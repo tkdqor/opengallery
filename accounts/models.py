62c570f35e8d984c1d760179385be0b53c4c3d47
sangbaek <aumsbk@naver.com>
2022-08-21 15:48:15 +0900

    Add : 개발환경 셋팅 및 기본 모델링 구축 - pipenv 가상환경 설정 - django 프로젝트 및 accounts, writers 앱 생성 - 린트, 포메터 설정 - User 커스텀 모델 설정 - Writer, Painting, Exhibition 모델 설정

---
+++ accounts/models.py
@@ -0,0 +1,89 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    """
+    Assignee : 상백
+
+    custom user model을 사용하기 위해 UserManager 클래스와 create_user, create_superuser 함수 정의
+    user 생성 시, email를 필수로 지정 
+    """
+
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError("Users must have an email")
+        user = self.model(
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """python manage.py createsuperuser 사용 시 해당 함수가 사용됨"""
+        user = self.create_user(
+            email=email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    """
+    Assignee : 상백
+
+    User 모델을 커스텀해서 생성하는 클래스 정의
+    이메일, 이름, 성별, 연락처, 생년월일 필드 설정
+    """
+    id = models.BigAutoField(primary_key=True)
+    email = models.EmailField("이메일", max_length=100, unique=True)
+    password = models.CharField("비밀번호", max_length=128)
+    username = models.CharField("이름", max_length=16)
+    gender = models.CharField("성별", max_length=2)
+    mobile = models.CharField("연락처", max_length=20)
+    birth = models.DateField("생년월일")
+
+    """is_active가 False일 경우 계정이 비활성화됨"""
+    is_active = models.BooleanField("활성화", default=True)
+
+    """is_staff에서 해당 값 사용"""
+    is_admin = models.BooleanField("관리자", default=False)
+
+    created_at = models.DateTimeField("작성시간", auto_now_add=True)
+    updated_at = models.DateTimeField("수정시간", auto_now=True)
+
+    """"
+    # id로 사용 할 필드 지정.
+    # 로그인 시 USERNAME_FIELD에 설정 된 필드와 password가 사용된다.
+    """
+    USERNAME_FIELD = "email"
+
+    """user를 생성할 때 입력받을 필드 지정"""
+    REQUIRED_FIELDS = []
+
+    """custom user 생성 시 필요"""
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return f"email: {self.email} / name: {self.username}"
+
+    def has_perm(self, perm, obj=None):
+        """
+        # 로그인 사용자의 특정 테이블의 crud 권한을 설정, perm table의 crud 권한이 들어간다.
+        # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
+        """
+        return True
+
+    def has_module_perms(self, app_label):
+        """ "
+        # 로그인 사용자의 특정 app에 접근 가능 여부를 설정, app_label에는 app 이름이 들어간다.
+        # admin일 경우 항상 True, 비활성 사용자(is_active=False)의 경우 항상 False
+        """
+        return True
+
+    @property
+    def is_staff(self):
+        """admin 권한 설정"""
+        return self.is_admin
---
+++ writers/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from accounts.models import User
+
+class Writer(models.Model):
+    """
+    Assignee : 상백
+
+    User 모델과 1:1 관계가 형성되어 있는 Writer 작가 모델 정의
+    성별은 남/여 선택할 수 있도록 카테고리 설정
+    작가 등록 시 approval 필드 True로 변경하고 해당 작가와 문제가 있을 경우, False로 등록 보류 가능하도록 설정
+    """
+
+    class Category(models.TextChoices):
+        male = 'male'
+        female = 'female'
+
+    name = models.CharField("이름", max_length=16)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField("성별", max_length=10, choices=Category.choices)
+    birth = models.DateField("생년월일")
+    email = models.EmailField("이메일", max_length=100, unique=True)
+    mobile = models.CharField("연락처", max_length=20)
+    approval = models.BooleanField("작가등록여부", default=False)
+    created_at = models.DateTimeField("등록일자", auto_now_add=True)
+    updated_at = models.DateTimeField("수정일자", auto_now=True)  
+
+
+class Painting(models.Model):
+    """
+    Assignee : 상백
+
+    Writer 모델과 1:N 관계가 형성되어 있는 Painting 작품 모델 정의
+    가격은 0 이상, 호수는 1 이상 500 이하의 숫자를 설정하기 위해 django에서 제공하는 validator 사용
+    """
+
+    title = models.CharField("제목", max_length=64)
+    writer = models.ForeignKey("Writer", on_delete=models.CASCADE, related_name='writer_painting')
+    price = models.PositiveIntegerField("가격", default=0)
+    size = models.IntegerField("호수", validators=[MinValueValidator(1), MaxValueValidator(500)])
+    created_at = models.DateTimeField("등록일자", auto_now_add=True)
+    updated_at = models.DateTimeField("수정일자", auto_now=True)  
+
+
+class Exhibition(models.Model):
+    """
+    Assignee : 상백
+
+    Writer 모델과 1:N 관계가 형성되어 있는 Exhibition 전시 모델 정의
+    """
+
+    title = models.CharField("제목", max_length=64)
+    writer = models.ForeignKey("Writer", on_delete=models.CASCADE, related_name='writer_exhibition')
+    start = models.CharField("시작일", max_length=12)
+    end = models.CharField("종료일", max_length=12)
+    created_at = models.DateTimeField("등록일자", auto_now_add=True)
+    updated_at = models.DateTimeField("수정일자", auto_now=True)  
