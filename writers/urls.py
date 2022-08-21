69664d5a7663da87846412a2844d49b85c581584
sangbaek <aumsbk@naver.com>
2022-08-21 23:06:11 +0900

    Feat : #6 작품 목록 페이지 개발 - writers 앱 내부 view에 painting 함수로 로직 구현 - writers 앱에 작품 목록 조회 페이지 url 설정

--- writers/urls.py
+++ writers/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "writers"
 
 urlpatterns = [
-    # 작가 목록 조회 페이지
+    # 작가 목록 조회 페이지 url
     path("", views.list, name="list"),
+    # 작품 목록 조회 페이지 url
+    path("painting/", views.painting, name="painting"),
 ]
--- writers/views.py
+++ writers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Writer
+from .models import Painting, Writer
 
 
 # url : GET writers/
@@ -20,3 +20,22 @@ def list(request):
     }
 
     return render(request, "writers/list.html", context)
+
+
+# url : GET writers/painting/
+def painting(request):
+    """
+    Assignee : 상백
+
+    작품 목록을 조회할 수 있는 페이지 구현
+    최근에 등록된 순서대로 조회할 수 있게 ORM 메소드인 order_by를 created_at 내림차순으로 설정
+    작품 제목, 가격, 호수, 작가 정보 응답
+    """
+
+    paintings = Painting.objects.all().order_by("-created_at")
+
+    context = {
+        "paintings": paintings,
+    }
+
+    return render(request, "writers/painting.html", context)
