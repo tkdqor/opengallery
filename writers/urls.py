18596906a193df431ee9c1687745d48128b2cf4b
sangbaek <aumsbk@naver.com>
2022-08-25 02:20:29 +0900

    Feat : #10 관리자 페이지에서 대시보드 페이지 및 작가 등록 신청 내역 조회 페이지 개발 - writers 앱 내부 view에 admin 및 admin_register 함수로 로직 구현 - 관리자 대시보드 페이지 및 작가 등록 신청 내역 조회 페이지 url 설정, html 생성

--- writers/urls.py
+++ writers/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path("painting/", views.painting, name="painting"),
     # 작가 등록 신청 페이지 url
     path("register/", views.register, name="register"),
+    # 관리자 대시보드 페이지 url
+    path("admin/", views.admin, name="admin"),
+    # 작가 등록 신청 내역 조회 페이지 url
+    path("admin/register/", views.admin_register, name="admin_register"),
+    # 작가 통계 페이지 url
+    path("admin/statistics/", views.admin_statistics, name="admin_statistics"),
 ]
